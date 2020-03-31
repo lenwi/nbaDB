fb85d758a4a0416e8391bf1b84f31a6a3362897e
Wen Li <i1s2b@ugrad.cs.ubc.ca>
2020-03-30 19:53:06 -0700

    added variable query

---
+++ node/routes/newZscore.js
@@ -0,0 +1,19 @@
+module.exports = {
+    getNewZscore: (req, res) => {
+        var zInput=req.body.zscoreinput;
+        let query = "select * from teamplayers " + "where zscore >= " + zInput; //display players with teams
+
+        // execute query
+        db.query(query, (err, result) => {
+            if (err) {
+                res.redirect('/');
+            }
+            console.log("###########################")
+            console.log(res)
+            res.render('index.ejs', {
+                title: "NBA DB | View Players", 
+                players: result, 
+            });
+        });
+    },
+};
