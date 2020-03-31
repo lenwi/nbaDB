bb50220b69b2f92ef2b8519aedda5840eb6c9f81
Wen Li <i1s2b@ugrad.cs.ubc.ca>
2020-03-31 02:31:27 -0700

    added >= and < options for user

--- node/routes/newZscore.js
+++ node/routes/newZscore.js
@@ -1,7 +1,15 @@
 module.exports = {
     getNewZscore: (req, res) => {
         var zInput=req.body.zscoreinput;
-        let query = "select * from teamplayers " + "where zscore >= " + zInput; //display players with teams
+        var inputValue = req.body.submit;
+
+        let query = "";
+
+        if (inputValue == ">=") {
+            query = "select * from teamplayers " + "where zscore >= " + zInput; //display players with teams
+        } else {
+            query = "select * from teamplayers " + "where zscore < " + zInput; //display players with teams
+        }
 
         // execute query
         db.query(query, (err, result) => {
