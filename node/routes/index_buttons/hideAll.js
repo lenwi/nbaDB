d14bca9f226907f0128b30da4114aa07917455dc
Michael Zhou <36398173+michaelzhou24@users.noreply.github.com>
2020-04-01 23:10:27 -0700

    add simple aggregate

--- node/routes/index_buttons/hideAll.js
+++ node/routes/index_buttons/hideAll.js
@@ -1,16 +1,27 @@
 module.exports = {
     getHideAll: (req, res) => {
-        let query = "select teamname, playername, playernum, playerposition from teamplayers" //display players with teams
+        let query = "select teamname, playername, playernum, playerposition from teamplayers"; //display players with teams
 
-        // execute query
         db.query(query, (err, result) => {
             if (err) {
                 res.redirect('/');
             }
-            console.log("###########################")
-            res.render('indexHiddenAll.ejs', {
-                title: "NBA DB | View Players", 
-                players: result, 
+            db.query("select count(*) as pcount from teamplayers", (err1, result1) => {
+                if (err) {
+                    res.redirect('/');
+                }
+                var count;
+                if (result1.length < 1) {
+                    count = 0;
+                } else {
+                    count = result1[0].pcount;
+                }
+
+                res.render('indexHiddenAll.ejs', {
+                    title: "NBA DB | View Players",
+                    players: result,
+                    pcount: count
+                });
             });
         });
     },
